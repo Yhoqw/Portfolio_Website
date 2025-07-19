c8d4f7b4ae40fef775e6e93f5af7993ce211e0d7
Yhoqw <yazdanalikhan005@gmail.com>
2025-07-19 18:20:57 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+console.log("Portfolio Home Loaded");
+
+const glow = document.querySelector('.cursor-glow');        //Method that allows you to select HTML elements
+const dot = document.querySelector('.cursor-dot');
+ 
+//Cursor glow
+if (glow) {
+
+  document.addEventListener('mousemove', e => {
+    requestAnimationFrame(() => {   
+      glow.style.left = `${e.clientX}px`;
+      glow.style.top = `${e.clientY}px`;
+      dot.style.left = `${e.clientX}px`;
+      dot.style.top = `${e.clientY}px`;
+    }); });
+}
+
+//Hover effect for links
+if (dot) {
+    const links = document.querySelectorAll('a');
+    links.forEach(link => {
+        link.addEventListener('mouseover', () => {
+            dot.classList.add('hover');
+        });
+        link.addEventListener('mouseleave', () => {
+            dot.classList.remove('hover');
+        });
+    });
+
+    //Dot over text
+    const textElements = document.querySelectorAll('h1, h2, p, a');
+    textElements.forEach(text_element => {
+        text_element.addEventListener('mouseenter', () => dot.classList.add('text-hover'));
+        text_element.addEventListener('mouseleave', () => dot.classList.remove('text-hover'));
+    })
+}
+
+//typing animation for header
+const headerTextElement = document.querySelector('header p');
+if (headerTextElement) {
+    const text = headerTextElement.textContent;
+    headerTextElement.textContent = '';
+    let i = 0
+    let isDeleting = false;
+
+    function typeWriter() {
+        if (isDeleting) {
+            // Deleting
+            headerTextElement.textContent = text.substring(0, i - 1);
+            i--;
+            if (i === 0) {
+                isDeleting = false;
+            }
+        } 
+        else {
+            // Typing
+            headerTextElement.textContent = text.substring(0, i + 1);
+            i++;
+            if (i === text.length) {
+                isDeleting = true;
+            }
+        }
+
+        const typingSpeed = isDeleting ? 50 : 150;
+        const delay = i === text.length ? 2000 : typingSpeed; // Pause at the end
+        setTimeout(typeWriter, delay);        
+    }
+
+    typeWriter();
+}
