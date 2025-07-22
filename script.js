5a0b5ada02fbaa06dcd70eeecc9672a6d6eef86b
Yazdan Ali Khan <yazdanalikhan005@gmail.com>
2025-07-23 02:55:45 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,16 +2,19 @@ console.log("Portfolio Home Loaded");
 
 const glow = document.querySelector('.cursor-glow');        //Method that allows you to select HTML elements
 const dot = document.querySelector('.cursor-dot');
+const circle = document.querySelector('.cursor-circle');
  
 //Cursor glow
 if (glow) {
 
   document.addEventListener('mousemove', e => {
     requestAnimationFrame(() => {   
-      glow.style.left = `${e.clientX}px`;
-      glow.style.top = `${e.clientY}px`;
+      //glow.style.left = `${e.clientX}px`;
+      //glow.style.top = `${e.clientY}px`;
       dot.style.left = `${e.clientX}px`;
       dot.style.top = `${e.clientY}px`;
+      circle.style.left = `${e.clientX}px`;
+      circle.style.top = `${e.clientY}px`;
     }); });
 }
 
@@ -56,15 +59,30 @@ if (headerTextElement) {
             // Typing
             headerTextElement.textContent = text.substring(0, i + 1);
             i++;
-            if (i === text.length) {
-                isDeleting = true;
-            }
+            if (i === text.length) 
+            {    isDeleting = true;  }
         }
-
         const typingSpeed = isDeleting ? 50 : 150;
         const delay = i === text.length ? 2000 : typingSpeed; // Pause at the end
         setTimeout(typeWriter, delay);        
     }
-
     typeWriter();
 }
+
+// Click animation ripple effect
+function showClickAnimation(e) {
+    const ripple = document.createElement('div');
+    // Use the class we defined in our CSS for the animation
+    ripple.className = 'click-ripple';
+    document.body.appendChild(ripple);
+
+    // Position the ripple at the cursor's location
+    ripple.style.top = `${e.clientY}px`;
+    ripple.style.left = `${e.clientX}px`;
+
+    // Remove the ripple from the DOM after the animation is complete
+    ripple.addEventListener('animationend', () => {
+        document.body.removeChild(ripple);
+    });
+}
+document.addEventListener('click', showClickAnimation);
